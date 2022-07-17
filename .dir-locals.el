75f918129d6a3a95eb87ca5836f93bfea0cac8c4
grtcdr <ba.tahaaziz@gmail.com>
2022-07-17 19:08:18 +0100

    New post about PlantUML!

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,3 @@
 ((less-css-mode . ((less-css-lessc-command . "npm exec lessc")
 		   (less-css-compile-at-save . t)
-		   (eval . (progn (setq less-css-output-directory (grt/site-content-directory 'base "stylesheets")))))))
+		   (eval . (progn (setq less-css-output-directory (grt/site-build-directory 'base "stylesheets")))))))

4fa4c794ea1c3d2fadcb11a133968a6da0e6fb22
grtcdr <ba.tahaaziz@gmail.com>
2022-12-01 12:25:41 +0100

    Remove let expression in publish-sitemap-format-entry

--- lisp/publish.el
+++ lisp/publish.el
@@ -13,10 +13,6 @@
 
 ;; Set the current working directory to the project root
 
-(setq default-directory
-      (expand-file-name
-       (project-root (project-current))))
-
 ;;; My information:
 
 (setq user-full-name "Aziz Ben Ali"
@@ -37,12 +33,11 @@
 
 (defun publish-sitemap-format-entry (entry style project)
   "Format a sitemap entry with its date."
-  (let ((prefix (file-name-concat (project-root (project-current)) "posts")))
-    (format "%s - [[file:%s][%s]] %s"
-	    (format-time-string "%Y-%m-%d" (org-publish-find-date entry project))
-	    entry
-	    (org-publish-find-title entry project)
-	    (org-publish-find-property entry :filetags project 'site-html))))
+  (format "%s - [[file:%s][%s]] %s"
+	  (format-time-string "%Y-%m-%d" (org-publish-find-date entry project))
+	  entry
+	  (org-publish-find-title entry project)
+	  (org-publish-find-property entry :filetags project 'site-html)))
 
 (defun publish-get-template (path)
   "Read a template from the templates directory."

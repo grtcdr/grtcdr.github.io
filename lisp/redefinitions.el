0b6d00054c8fe6eb6d43e6bd3a9bbb5d137be466
grtcdr <tahaaziz.benali@esprit.tn>
2023-01-12 11:09:56 +0100

    General changes

--- lisp/redefinitions.el
+++ lisp/redefinitions.el
@@ -5,7 +5,7 @@
   "Return format specification for preamble and postamble.
 INFO is a plist used as a communication channel."
   `((?a . ,(org-export-data (plist-get info :author) info))
-    (?t (org-export-data (plist-get info :title) info))
+    (?t . ,(org-export-data (plist-get info :title) info))
     (?m . ,(plist-get info :email))
     (?e . ,(liaison-get-resource-url 'edit))
     (?l . ,(liaison-get-resource-url 'log))))

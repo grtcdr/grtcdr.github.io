4b6afe09e067f1df7cae9ad2e10733e2fda75c51
grtcdr <tahaaziz.benali@esprit.tn>
2023-01-13 21:28:44 +0100

    Move back redefinitions to publish.el

--- lisp/publish.el
+++ lisp/publish.el
@@ -30,12 +30,8 @@
 (require 'org-id)
 ;; XML templating
 (require 'shr)
-;; Project management
-(require 'project)
 ;; URL generation
 (require 'liaison)
-;; Built-in function redefinitions
-(require 'redefinitions)
 
 (defun env/enabled? (env)
   (length> (member env '("yes" "true")) 0))
@@ -146,6 +142,15 @@ dotfiles publishing project."
 			   (type . "image/x-icon")
 			   (href . "/assets/favicon.ico")))))
   "HTML headers shared across publishing projects.")
+
+(defun org-html-format-spec (info)
+  "Return format specification for preamble and postamble.
+INFO is a plist used as a communication channel."
+  `((?a . ,(org-export-data (plist-get info :author) info))
+    (?t . ,(org-export-data (plist-get info :title) info))
+    (?m . ,(plist-get info :email))
+    (?e . ,(liaison-get-resource-url 'edit))
+    (?l . ,(liaison-get-resource-url 'log))))
 
 (setq org-publish-project-alist
       (let* ((posts-postamble (concat (s/get-template "postamble/posts.html")
